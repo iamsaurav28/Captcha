215c763ecf9ff327196f07300ccd600891eb01a5
saurav darji <sauravdarji8793@gmail.com>
2023-03-26 20:05:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Captcha from './Components/Captcha';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Captcha />
       </header>
     </div>
   );
---
+++ src/Components/Captcha.js
@@ -0,0 +1,101 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import captchaImg from '../Images/captcha.jpg';
+import studentIMG from '../Images/user.png';
+import React, { useState, useEffect } from 'react';
+function Captcha() {
+
+     const[userName, setUserName] = useState("")
+
+
+  const [user, setUser] = useState({
+      username:""
+  });
+  const characters ='abc123';
+  function generateString(length) 
+  {
+      let result = '';
+      const charactersLength = characters.length;
+      for ( let i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+     return result;
+   }
+   const captcha = generateString(6) // Function called here and save in captcha variable
+   let handleChange = (e) => {
+     let name = e.target.name;
+     let value = e.target.value;
+     user[name] = value;
+     setUser(user);
+  }
+  const onSubmit = e => {
+    var element =  document.getElementById("succesBTN");
+    var inputData = document.getElementById("inputType");
+     element.style.cursor = "wait";
+     element.innerHTML  = "Checking...";
+     inputData.disabled = true;
+     element.disabled = true;
+      var myFunctions = function(){
+          if(captcha == user.username)
+          {
+            element.style.backgroundColor   = "green";
+            element.innerHTML  = "Captcha Verified";
+            element.disabled = true;
+            element.style.cursor = "not-allowed";
+            inputData.style.display = "none";
+            
+          }
+          else
+          {
+            element.style.backgroundColor   = "red";
+            element.style.cursor = "not-allowed";
+            element.innerHTML  = "Not Matched";
+            element.disabled = true;
+            //  element.disabled = true;
+            var myFunction = function(){
+              element.style.backgroundColor   = "#007bff";
+              element.style.cursor = "pointer";
+              element.innerHTML  = "Verify Captcha";
+              element.disabled = false;
+              inputData.disabled = false;
+              inputData.value ='sssss';
+            };
+            setTimeout(myFunction,5000);
+          }
+        }   
+        setTimeout(myFunctions,5000); 
+  };
+  
+   return (
+    <div className="container">
+      <h4><b> Register Here</b></h4>
+      <div>
+          <div>
+            <h4 id="captcha" style={{ marginTop:"370px",marginLeft:"25px",position:"absolute"}}>{captcha}</h4>
+            
+            <div>
+            <input type="text" placeholder=' Full Name...' onChange={(e) => setUserName(e.target.value)} /> <hr/>
+               <input type="email" placeholder='Email...'/> <hr/>
+               <input type="password" placeholder='password...'/><hr/>
+               <input type="password" placeholder='confirm password...'/>
+            </div>
+
+            <div>
+              <img src={studentIMG} className="student-img" height="90"/> <span className="font-weight-bold ml-3"  style={{ marginTop:"50px"}} >{userName}</span>
+            </div>
+            <div>
+              <img src={captchaImg} className="mt-3 mb-3" height="50"/> 
+            </div>
+            <div>
+              <input type="text" id="inputType" className="form-control"placeholder="Enter Captcha"
+                name="username"  onChange={handleChange} autocomplete="off" style={{width:"20%"}}
+                />
+              <button type="button" id="succesBTN" onClick={onSubmit} className="btn btn-primary ml-1">Verify Captcha</button>
+            
+            </div>
+        
+           </div>
+        </div>
+      </div>
+    );
+}
+export default Captcha;
